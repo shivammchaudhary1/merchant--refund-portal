4923c0e1c225458c6d357ce631c36903cfeae940
Shivam Chaudhary <shivamchaudhary@Shivams-MacBook-Air-2.local>
2026-03-10 08:05:17 -0500

    fixed

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  FRONTEND_URL: import.meta.env.VITE_FRONTEND_URL || "http://localhost:5173",
-  BACKEND_API: import.meta.env.VITE_BACKEND_API || "http://localhost:8080",
+  FRONTEND_URL: import.meta.env.VITE_FRONTEND_URL,
+  BACKEND_API: import.meta.env.VITE_BACKEND_API,
 };
 
 export default config;
